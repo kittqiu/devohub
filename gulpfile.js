8075ab55a9298c2fffb007cb733315466ae07ec4
kittqiu <kittqiu@163.com>
2017-02-07 18:49:10 +0800

    增加合并js的功能

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var
+    _ = require('lodash'),
+    fs = require('fs'),
+    less = require('gulp-less'),
+    jslint = require('gulp-jslint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    gulp = require('gulp'),
+    babel = require('gulp-babel');
+
+var theme = 'default';
+
+function getJavaScriptFiles(file) {
+    var
+        re = /^.*\<script\s+src\=\"(.*)\"\>.*$/,
+        data = fs.readFileSync(file, { encoding: 'utf-8' }),
+        begin = data.indexOf('<!-- BEGIN JAVASCRIPT COMPRESS -->'),
+        end = data.indexOf('<!-- END JAVASCRIPT COMPRESS -->'),
+        lines;
+    if (begin === (-1) || end === (-1) || begin > end) {
+        throw 'Error: special comment not found!';
+    }
+    lines = data.substring(begin, end).split('\n');
+    lines = _.map(lines, function (line) {
+        var m = re.exec(line);
+        if (m) {
+            return m[1].replace(/\{\{\s*\_\_theme\_\_\s*\}\}/, theme);
+        }
+        return null;
+    });
+    lines = _.filter(lines, function (line) {
+        return line !== null;
+    });
+    return _.map(lines, function (line) {
+        return '.' + line;
+    });
+}
+
+//console.log(getJavaScriptFiles('./view/theme/' + theme + '/_base.html'));
+
+gulp.task('jslint', function () {
+    return gulp.src([
+        './controllers/*.js',
+        './models/*.js',
+        './search/*.js',
+        './*.js'
+    ]).pipe(jslint({
+        node: true,
+        nomen: true,
+        sloppy: true,
+        plusplus: true,
+        unparam: true,
+        stupid: true
+    }));
+});
+
+gulp.task('uglify', function () {
+    var jsfiles = getJavaScriptFiles('./view/_base.html');
+    return gulp.src(jsfiles)
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./static/js'));
+});
+
+
+gulp.task('default', ['uglify']);
+
